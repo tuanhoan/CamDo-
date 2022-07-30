149062ee48bada0c1db1605352728e8fbf694df5
Lê Châu <lebaochau97@gmail.com>
2022-07-30 17:34:52 +0700

    add page feature and payment

--- Source/CamDo/BaseSource.WebApp/Controllers/HomeController.cs
+++ Source/CamDo/BaseSource.WebApp/Controllers/HomeController.cs
@@ -27,6 +27,18 @@ namespace BaseSource.WebApp.Controllers
         }
 
         [AllowAnonymous]
+        public IActionResult Feature()
+        {
+            return View();
+        }
+
+        [AllowAnonymous]
+        public IActionResult Payment()
+        {
+            return View();
+        }
+
+        [AllowAnonymous]
         public IActionResult Privacy()
         {
             return View();
