9dec75a71db34ce7d354dc3af7541f486bf98d31
Lê Châu <lebaochau97@gmail.com>
2022-08-02 00:03:23 +0700

    add page lien he

---
+++ Source/CamDo/BaseSource.Data/Migrations/20220801140545_LineHeTable.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BaseSource.Data.Migrations
+{
+    public partial class LineHeTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "LienHes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Phone = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Message = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CreatedTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    IsRead = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_LienHes", x => x.Id);
+                });
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "c1105ce5-9dbc-49a9-a7d5-c963b6daa62a",
+                column: "ConcurrencyStamp",
+                value: "ce62369e-d4d7-42bd-b797-f9b322f2b5fe");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "ffded6b0-3769-4976-841b-69459049a62d",
+                columns: new[] { "ConcurrencyStamp", "PasswordHash" },
+                values: new object[] { "6651a8b0-9016-4c8a-82db-bef24b33122c", "AQAAAAEAACcQAAAAEGniIOzNrtHwB0WFYyFuxCnAv8DbyLPV5gOe67MU+MrKB78syTAK3uld/YaISAlMAQ==" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "LienHes");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "c1105ce5-9dbc-49a9-a7d5-c963b6daa62a",
+                column: "ConcurrencyStamp",
+                value: "32423668-7ef8-42bf-ab7e-1087583f1f0b");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "ffded6b0-3769-4976-841b-69459049a62d",
+                columns: new[] { "ConcurrencyStamp", "PasswordHash" },
+                values: new object[] { "f41c2e68-9619-4306-90f3-72d2272189b9", "AQAAAAEAACcQAAAAENTp4R8P565EzMJS3dTR0/WrtxPt5L8nrULMMu8qJAgYKm759Vncbuqi21UIRxff8g==" });
+        }
+    }
+}
---
+++ Source/CamDo/BaseSource.ViewModels/LienHe/LienHeVm.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BaseSource.ViewModels.LienHe
+{
+    public class CreateLienHeVm
+    {
+        [Display(Name = "Họ và tên")]
+        [Required(ErrorMessage = "Vui lòng nhập họ và tên")]
+        public string Name { get; set; }
+
+        [Display(Name = "Email")]
+        [Required(ErrorMessage = "Vui lòng nhập email")]
+        public string Email { get; set; }
+
+        [Display(Name = "Số điện thoại")]
+        [Required(ErrorMessage = "Vui lòng nhập số điện thoại")]
+        public string Phone { get; set; }
+
+        [Display(Name = "Nội dung")]
+        [Required(ErrorMessage = "Vui lòng nhập nội dung")]
+        public string Message { get; set; }
+    }
+}
