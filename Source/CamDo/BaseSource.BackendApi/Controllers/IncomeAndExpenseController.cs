79fb182f5cccf8534c410d49943a7f30bb43547f
phamminhyen97 <phamminhyen97@gmail.com>
2022-08-03 14:55:47 +0700

    update menu sidebar page admin

---
+++ Source/CamDo/BaseSource.BackendApi/Controllers/IncomeAndExpenseController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BaseSource.BackendApi.Controllers
+{
+    public class IncomeAndExpenseController : Controller
+    {
+        public async Task<IActionResult> Income()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> Expense()
+        {
+            return View();
+        }
+    }
+}
