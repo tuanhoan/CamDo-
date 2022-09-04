e7890f594b3e10b4437ee394f4e0f6bd91c68ba8
tuanhoan <ntanh.hutech@gmail.com>
2022-09-04 21:46:01 +0700

    commit

---
+++ Source/CamDo/BaseSource.BackendApi/Controllers/BaoCaosController.cs
@@ -0,0 +1,80 @@
+﻿using BaseSource.Data.EF;
+using BaseSource.ViewModels.BaoCao;
+using BaseSource.ViewModels.Common;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BaseSource.BackendApi.Controllers
+{
+    public class BaoCaosController : BaseApiController
+    {
+        private readonly BaseSourceDbContext _db;
+        public BaoCaosController(BaseSourceDbContext db)
+        {
+            _db = db;
+        }
+
+        [HttpGet("ReportBalance")]
+        public async Task<IActionResult> ReportBalance()
+        {
+            var hopdongs = await _db.HopDongs.ToListAsync();
+            var cuahangtranLogs = await _db.CuaHang_TransactionLogs.ToListAsync();
+            var cuahangQuyTienLogs = await _db.CuaHang_QuyTienLogs.ToListAsync();
+            var data = new ReportBalanceVM()
+            {
+                TienDauNgay = new Summary
+                {
+                    Thu = 0,
+                    Chi = 0,
+                },
+                BatHo = new Summary
+                {
+                    Thu = 0,
+                    Chi = 0,
+                },
+                CamDo = new Summary
+                {
+                    Thu = hopdongs.Where(x => x.HD_Loai == Shared.Enums.ELoaiHopDong.Camdo).Sum(x => x.TongTienDaThanhToan),
+                    Chi = hopdongs.Where(x => x.HD_Loai == Shared.Enums.ELoaiHopDong.Camdo).Sum(x => x.HD_TongTienVayBanDau),
+                },
+                ChiHoatDong = new Summary
+                {
+                    Thu = cuahangtranLogs.Where(x => x.FeatureType == Shared.Enums.EFeatureType.Chi).Sum(x => x.MoneySub),
+                    Chi = cuahangtranLogs.Where(x => x.FeatureType == Shared.Enums.EFeatureType.Chi).Sum(x => x.MoneyAdd),
+                },
+                NguonVon = new Summary
+                {
+                    Thu = hopdongs.Where(x => x.HD_Loai == Shared.Enums.ELoaiHopDong.GopVon).Sum(x => x.TienLaiToiNgayHienTai),
+                    Chi = hopdongs.Where(x => x.HD_Loai == Shared.Enums.ELoaiHopDong.GopVon).Sum(x => x.HD_TongTienVayBanDau),
+                },
+                ThuHoatDong = new Summary
+                {
+                    Thu = cuahangtranLogs.Where(x => x.FeatureType == Shared.Enums.EFeatureType.Thu).Sum(x => x.MoneySub),
+                    Chi = cuahangtranLogs.Where(x => x.FeatureType == Shared.Enums.EFeatureType.Thu).Sum(x => x.MoneyAdd),
+                },
+                TienMatConLai = new Summary
+                {
+                    Thu = 0,
+                    Chi = 0,
+                },
+                VayLai = new Summary
+                {
+                    Thu = hopdongs.Where(x => x.HD_Loai == Shared.Enums.ELoaiHopDong.Vaylai).Sum(x => x.TienLaiToiNgayHienTai),
+                    Chi = hopdongs.Where(x => x.HD_Loai == Shared.Enums.ELoaiHopDong.Vaylai).Sum(x => x.HD_TongTienVayBanDau),
+                },
+            };
+            data.TienDauNgay.Total = cuahangQuyTienLogs.Where(x => x.LogType == Shared.Enums.EQuyTienCuaHang_LogType.TienDauNgay).Sum(x => x.Money);
+            data.BatHo.Total = data.BatHo.Chi - data.BatHo.Thu;
+            data.CamDo.Total = data.CamDo.Chi - data.CamDo.Thu;
+            data.ChiHoatDong.Total = data.ChiHoatDong.Chi - data.ChiHoatDong.Thu;
+            data.NguonVon.Total = data.NguonVon.Chi - data.NguonVon.Thu;
+            data.ThuHoatDong.Total = data.ThuHoatDong.Chi - data.ThuHoatDong.Thu;
+            data.TienMatConLai.Total = data.TienMatConLai.Chi - data.TienMatConLai.Thu;
+            data.VayLai.Total = data.VayLai.Chi - data.VayLai.Thu;
+
+            return base.Ok(new ApiSuccessResult<ReportBalanceVM>(data));
+        }
+    }
+}
---
+++ Source/CamDo/BaseSource.ViewModels/BaoCao/ReportBalanceVM.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaseSource.ViewModels.BaoCao
+{
+    public class ReportBalanceVM
+    {
+        public Summary TienDauNgay { get; set; }
+        public Summary CamDo { get; set; }
+        public Summary VayLai { get; set; }
+        public Summary BatHo { get; set; }
+        public Summary ThuHoatDong { get; set; }
+        public Summary ChiHoatDong { get; set; }
+        public Summary NguonVon { get; set; }
+        public Summary TienMatConLai { get; set; }
+    }
+    public class Summary
+    {
+        public double Thu { get; set; }
+        public double Chi { get; set; }
+        public double Total { get; set; }
+    }
+}
